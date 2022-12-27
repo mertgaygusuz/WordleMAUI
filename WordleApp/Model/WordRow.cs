58a638dd5db89a15a15507ded2f8acfcde34c0b8
mert.gaygusuz <mert.gaygusuz@vendrops.com>
2022-12-27 23:09:28 +0300

    first commit

--- WordleApp/Model/WordRow.cs
+++ WordleApp/Model/WordRow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace WordleApp.Model
 {
     public class WordRow
     {
+        public Letter[] Letters { get; set; }
+        public void Validate(char[] correctAnswer)
+        {
+            //döngüler burada olacak
+        }
+        
+    }
+
+    public partial class Letter : ObservableObject
+    {
+        [ObservableProperty]
+        private char input;
+
+        [ObservableProperty]
+        private Color color;
+        public Color Color { get; set; }
     }
 }
---
+++ WordleApp/ViewModel/GameViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using WordleApp.Model;
+
+namespace WordleApp.ViewModel
+{
+    public partial class GameViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private WordRow[] rows;
+
+        [ICommand]
+        public void Enter()
+        {
+
+        }
+    }
+}
